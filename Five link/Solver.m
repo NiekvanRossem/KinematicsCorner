38ab7349667ba96938d3245d703f30ece32b8bcb
Niek <niek.vanrossem@hotmail.com>
2024-11-16 18:29:57 +0100

    Implemented roll steer graph
    General script now fully functional

--- Five link/Solver.m
+++ Five link/Solver.m
@@ -9,7 +9,7 @@
 %% Prepare workspace
 
 % clear everything
-%clear; close all; clc;
+clear; close all; clc;
 
 % set all figures to docked mode
 set(0,'DefaultFigureWindowStyle','docked');
@@ -26,13 +26,15 @@ Settings.MaxStep = 1;
 Settings.Axle = "Front";
 Settings.Animation = "on";
 
-%% DUT24 PUPs
+%% Load suspension hardpoints
 
 % load suspension hardpoints
-[Car, PUP] = DUT25_PUP(Settings);
+[Car, PUP] = Suh_PUP(Settings);
 
 % find loaded radius
 Car.RL = abs(PUP.r_WC_O(3) - PUP.r_CP_O(3));
+
+%% Solve
 
 % create state vector
 r = vertcat( ...
@@ -80,8 +82,8 @@ output.dTravel = [];
 q_out = zeros(numel(output.t),6);
 
 % loop over all time steps
-for i = 1:numel(output.t)
-    [~, q_out(i,:)] = FiveLink(output.t(i), transpose(r_out(i,:)));
+for n = 1:numel(output.t)
+    [~, q_out(n,:)] = FiveLink(output.t(n), transpose(r_out(n,:)));
 end
 
 % generalised DoF
@@ -92,7 +94,7 @@ output.dCamber     = q_out(:,3);
 output.dSpin       = q_out(:,4);
 output.dToe        = q_out(:,5);
 
-clear i; clear r_out; clear q_out; clear t; clear dTravel;
+clear r_out; clear q_out; clear t; clear dTravel;
 
 % extract actual track width
 output.Track = 2*output.r_CP_O_y;
@@ -100,22 +102,24 @@ output.Track = 2*output.r_CP_O_y;
 % extract wheel travel
 output.Travel = output.r_CP_O_z;
 
-% calculate equivalent roll angle
+% calculate equivalent roll angle and roll rate
 output.Roll   = 180/pi*atan(output.Travel./(0.5*output.Track));
+output.dRoll  = 180/pi*atan(output.dTravel./(0.5*output.Track));
 
 % calculate ride height
 output.RideHeight = Car.RH - output.Travel;
 
 % Suspension params
-output.RollCentre  = output.Track./2.*(output.dTrack./output.dTravel);          % mm
-output.CamberGainB = -output.dCamber./output.dTravel*180/pi;             % deg/mm
-output.BumpSteer   = output.dToe./output.dTravel*180/pi;                 % deg/mm
-output.CasterGain  = -output.dSpin./output.dTravel*180/pi;               % deg/mm
-output.AntiRoll    = atan(output.dTrack./output.dTravel)*180/pi;         % deg
-output.AntiPitch   = atan(-output.dWheelbase./output.dTravel)*180/pi;    % deg
-output.LatGrad     = output.dTrack./output.dTravel;                      % mm/mm
-output.LongGrad    = output.dWheelbase./output.dTravel;                  % mm/mm
-output.CamberGainR = output.CamberGainB.*0.5.*output.Track.*sind(1)+1;   % deg/deg
+output.RollCentre   = output.Track./2.*(output.dTrack./output.dTravel);     % mm
+output.CamberGainB  = -output.dCamber./output.dTravel*180/pi;               % deg/mm
+output.BumpSteer    = output.dToe./output.dTravel*180/pi;                   % deg/mm
+output.CasterGain   = -output.dSpin./output.dTravel*180/pi;                 % deg/mm
+output.AntiRoll     = atan(output.dTrack./output.dTravel)*180/pi;           % deg
+output.AntiPitch    = atan(-output.dWheelbase./output.dTravel)*180/pi;      % deg
+output.LatGrad      = output.dTrack./output.dTravel;                        % mm/mm
+output.LongGrad     = output.dWheelbase./output.dTravel;                    % mm/mm
+output.CamberGainR  = output.CamberGainB.*0.5.*output.Track.*sind(1)+1;     % deg/deg
+output.RollSteer    = 180/pi*output.dToe./output.dRoll;                      % deg/deg
 
 % integrate to find camber and toe curves
 output.Camber = cumtrapz(output.t, -output.dCamber*180/pi);
@@ -124,95 +128,139 @@ output.Toe    = cumtrapz(output.t, output.dToe*180/pi);
 %% Plot output characteristics
 
 % camber
-figure(1);%"Name", "Camber characteristic");
-subplot(1,2,1); hold all;
-    title('Camber curve');
+figure(1);
+subplot(1,2,1); hold all; box on; grid minor;
+    title({'Camber curve', 'With respect to the chassis'});
     plot(output.Camber, output.Travel, '.', 'MarkerSize', 2);
     xlabel('Camber angle (deg)');
     ylabel('Travel (mm)');
     xline(0, 'k-'); yline(0, 'k-');
-    xlim([-5 5]); grid minor;
-subplot(1,2,2); hold all;
-    title('Camber gain');
+    xlim([-5 5]);
+subplot(1,2,2); hold all; box on; grid minor;
+    title('Camber gain in bump');
     plot(output.CamberGainB, output.Travel, '.', 'MarkerSize', 2);
     xlabel('Camber gain (deg/mm)');
     ylabel('Travel (mm)');
-    xline(0, 'k-'); yline(0, 'k-');
-    xlim([-0.1 -0.05]);  grid minor;
-
+    xline(0, 'k-');
+    
 % bump steer
-figure(2);%"Name", "Bumpsteer characteristic");
-subplot(1,2,1); hold all;
+figure(2);
+subplot(1,2,1); hold all; box on; grid minor;
     title('Bumpsteer curve');
     plot(output.Toe, output.Travel, '.', 'MarkerSize', 2);
     xlabel('Toe angle (deg)');
     ylabel('Travel (mm)');
     xline(0, 'k-'); yline(0, 'k-');
-    xlim([-0.5 0.5]); grid minor;
+    xlim([-0.5 0.5]);
     annotation('textarrow', 0.5*[0.85 0.9],[0.4 0.4], 'String', 'toe in');
     annotation('textarrow', 0.5*[0.35 0.3],[0.4 0.4], 'String', 'toe out');
-subplot(1,2,2); hold all;
+subplot(1,2,2); hold all; box on; grid minor;
     title('Toe gain');
     plot(output.BumpSteer, output.Travel, '.', 'MarkerSize', 2);
     xlabel('Toe gain (deg/mm)');
     ylabel('Travel (mm)');
     xline(0, 'k-'); yline(0, 'k-');
-    xlim([-15e-3 15e-3]); grid minor;
+    xlim([-15e-3 15e-3]);
 
-figure(3);%"Name", "Roll centre height"); hold all; grid minor;
+% roll centre height
+figure(3);
 title('Roll centre curve');
+hold all; box on; grid minor;
 plot(output.RideHeight, output.RollCentre, '.', 'MarkerSize', 2);
+xline(Car.RH, 'k-'); yline(0, 'k-');
 xlabel('Ride height (mm)');
 ylabel('Roll centre height (mm)');
 
-figure(4);%"Name", "Anti pitch angle");
-subplot(1,2,1); hold all; grid minor;
-    title('Lateral n-line angle curve');
+% n-line angles
+figure(4);
+subplot(1,2,1); hold all; box on; grid minor;
+    title({'Lateral n-line angle curve', 'With respect to the chassis'});
     plot(output.RideHeight, output.AntiRoll, '.', 'MarkerSize', 2);
+    xline(Car.RH, 'k-'); yline(0, 'k-');
     xlabel('Ride height (mm)');
     ylabel('Anti-roll angle (deg)');
-subplot(1,2,2); hold all; grid minor;
-    title('Longitudinal n-line angle curve');
+subplot(1,2,2); hold all; box on; grid minor;
+    title({'Longitudinal n-line angle curve', 'With respect to the chassis'});
     plot(output.RideHeight, output.AntiPitch, '.', 'MarkerSize', 2);
+    xline(Car.RH, 'k-'); yline(0, 'k-');
     xlabel('Ride height (mm)');
     ylabel('Anti-pitch angle (deg)');
 
+% camber in roll
+figure(5); 
+subplot(1,2,1); hold all; box on; grid minor;
+    title({'Camber angle versus roll angle', 'With respect to the ground plane'});
+    plot(output.Roll, output.Camber+output.Roll);
+    xline(0, 'k-'); yline(0, 'k-');
+    ylabel("Camber (deg)");
+    xlabel("Roll angle (deg)");
+subplot(1,2,2); hold all; box on; grid minor;
+    title({'Camber gain in roll'});
+    plot(output.Roll, output.CamberGainR); 
+    xline(0, 'k-');
+    ylabel("Camber gain in roll (deg/deg)");
+    xlabel("Roll angle (deg)");
+
+figure(6); 
+subplot(1,2,1); hold all; box on; grid minor;
+    title('Steer versus roll angle');
+    plot(output.Roll, output.Toe);
+    xlabel('Roll angle (deg)');
+    ylabel('Steer angle (deg)');
+    xline(0, 'k-'); yline(0, 'k-');
+subplot(1,2,2); hold all; box on; grid minor;
+    title('Roll steer coefficient over roll angle');
+    plot(output.Roll, output.RollSteer);
+    xlabel('Roll angle (deg)');
+    ylabel('Roll steer (deg/deg)');
+    xline(0, 'k-'); yline(0, 'k-');
+
 %% Plot animation
+
 if Settings.Animation == "on"
-    dynamicData = r_out(1,:);
     
-    new_FUB = [output.r_P1o_x, output.r_P1o_y, output.r_P1o_z];
-    new_FLB = [output.r_P3o_x, output.r_P3o_y, output.r_P3o_z];
-    new_FOT = [output.r_P5o_x, output.r_P5o_y, output.r_P5o_z];
-    new_CP_O = [output.r_CP_O_x, output.r_CP_O_y, output.r_CP_O_z];
+    % initialise outer PUP locations (only works with DWB for now)
+    Animation.new_P1o = [output.r_P1o_x(1), output.r_P1o_y(1), output.r_P1o_z(1)];
+    Animation.new_P2o = [output.r_P2o_x(1), output.r_P2o_y(1), output.r_P2o_z(1)];
+    Animation.new_P3o = [output.r_P3o_x(1), output.r_P3o_y(1), output.r_P3o_z(1)];
+    Animation.new_P4o = [output.r_P4o_x(1), output.r_P4o_y(1), output.r_P4o_z(1)];
+    Animation.new_P5o = [output.r_P5o_x(1), output.r_P5o_y(1), output.r_P5o_z(1)];
+    Animation.new_CP_O = [output.r_CP_O_x(1), output.r_CP_O_y(1), output.r_CP_O_z(1)];
     
-    f = figure("Name", "Animation"); clf; view(90,0); hold all;
+    % initialise figure
+    Animation.f = figure("Name", "Animation"); clf; view(135,30); hold all;
     box on; grid minor; axis equal;
-    xlim([-150 150]); ylim([150 650]); zlim([-50 400]);
+    %xlim([-150 150]); ylim([150 650]); zlim([-50 400]);
+    xlabel("X (mm)"); ylabel("Y (mm)"); zlabel("Z (mm)"); 
+
+    % plot links
+    Animation.h1 = plot3([Animation.new_P1o(1) PUP.r_P1i(1)], [Animation.new_P1o(2) PUP.r_P1i(2)], [Animation.new_P1o(3) PUP.r_P1i(3)], 'o-', 'Color', 'red');
+    Animation.h2 = plot3([Animation.new_P2o(1) PUP.r_P2i(1)], [Animation.new_P2o(2) PUP.r_P2i(2)], [Animation.new_P2o(3) PUP.r_P2i(3)], 'o-', 'Color', 'red');
+    Animation.h3 = plot3([Animation.new_P3o(1) PUP.r_P3i(1)], [Animation.new_P3o(2) PUP.r_P3i(2)], [Animation.new_P3o(3) PUP.r_P3i(3)], 'o-', 'Color', 'red');
+    Animation.h4 = plot3([Animation.new_P4o(1) PUP.r_P4i(1)], [Animation.new_P4o(2) PUP.r_P4i(2)], [Animation.new_P4o(3) PUP.r_P4i(3)], 'o-', 'Color', 'red');
+    Animation.h5 = plot3([Animation.new_P5o(1) PUP.r_P5i(1)], [Animation.new_P5o(2) PUP.r_P5i(2)], [Animation.new_P5o(3) PUP.r_P5i(3)], 'o-', 'Color', 'red');
     
-    h1 = plot3([new_FUB(1) PUP.r_FUIF(1)], [new_FUB(2) PUP.r_FUIF(2)], [new_FUB(3) PUP.r_FUIF(3)], 'o-', 'Color', 'red');
-    h2 = plot3([new_FUB(1) PUP.r_FUIR(1)], [new_FUB(2) PUP.r_FUIR(2)], [new_FUB(3) PUP.r_FUIR(3)], 'o-', 'Color', 'red');
-    h3 = plot3([new_FLB(1) PUP.r_FLIF(1)], [new_FLB(2) PUP.r_FLIF(2)], [new_FLB(3) PUP.r_FLIF(3)], 'o-', 'Color', 'red');
-    h4 = plot3([new_FLB(1) PUP.r_FLIR(1)], [new_FLB(2) PUP.r_FLIR(2)], [new_FLB(3) PUP.r_FLIR(3)], 'o-', 'Color', 'red');
-    h5 = plot3([new_FOT(1) PUP.r_FIT(1)], [new_FOT(2) PUP.r_FIT(2)], [new_FOT(3) PUP.r_FIT(3)], 'o-', 'Color', 'red');
-    h6 = plot3(new_CP_O(1), new_CP_O(2), new_CP_O(3), 'ro');
+    % plot tyre contact patch
+    Animation.h6 = plot3(Animation.new_CP_O(1), Animation.new_CP_O(2), Animation.new_CP_O(3), 'ro');
     
-    for i = 1:length(r_out(:,1))
+    % update data in figure
+    for n = 1:length(output.t)
     
-        dynamicData = r_out(i,:);
-    
-        new_FUB = dynamicData(1:3);
-        new_FLB = dynamicData(4:6);
-        new_FOT = dynamicData(19:21);
-        new_CP_O = dynamicData(25:27);
+        % find new PUP coordinates
+        Animation.new_P1o = [output.r_P1o_x(n), output.r_P1o_y(n), output.r_P1o_z(n)];
+        Animation.new_P2o = [output.r_P2o_x(n), output.r_P2o_y(n), output.r_P2o_z(n)];
+        Animation.new_P3o = [output.r_P3o_x(n), output.r_P3o_y(n), output.r_P3o_z(n)];
+        Animation.new_P4o = [output.r_P4o_x(n), output.r_P4o_y(n), output.r_P4o_z(n)];
+        Animation.new_P5o = [output.r_P5o_x(n), output.r_P5o_y(n), output.r_P5o_z(n)];
+        Animation.new_CP_O = [output.r_CP_O_x(n), output.r_CP_O_y(n), output.r_CP_O_z(n)];
     
         % update data
-        set(h1, 'XData', [new_FUB(1) PUP.r_P1i(1)], 'YData', [new_FUB(2) PUP.r_FUIF(2)], 'ZData', [new_FUB(3) PUP.r_FUIF(3)]);
-        set(h2, 'XData', [new_FUB(1) PUP.r_P2i(1)], 'YData', [new_FUB(2) PUP.r_FUIR(2)], 'ZData', [new_FUB(3) PUP.r_FUIR(3)]);
-        set(h3, 'XData', [new_FLB(1) PUP.r_P3i(1)], 'YData', [new_FLB(2) PUP.r_FLIF(2)], 'ZData', [new_FLB(3) PUP.r_FLIF(3)]);
-        set(h4, 'XData', [new_FLB(1) PUP.r_P4i(1)], 'YData', [new_FLB(2) PUP.r_FLIR(2)], 'ZData', [new_FLB(3) PUP.r_FLIR(3)]);
-        set(h5, 'XData', [new_FOT(1) PUP.r_P5i(1)], 'YData', [new_FOT(2) PUP.r_FIT(2)], 'ZData', [new_FOT(3) PUP.r_FIT(3)]);
-        set(h6, 'XData', new_CP_O(1), 'YData', new_CP_O(2), 'ZData', new_CP_O(3));
+        set(Animation.h1, 'XData', [Animation.new_P1o(1) PUP.r_P1i(1)], 'YData', [Animation.new_P1o(2) PUP.r_P1i(2)], 'ZData', [Animation.new_P1o(3) PUP.r_P1i(3)]);
+        set(Animation.h2, 'XData', [Animation.new_P2o(1) PUP.r_P2i(1)], 'YData', [Animation.new_P2o(2) PUP.r_P2i(2)], 'ZData', [Animation.new_P2o(3) PUP.r_P2i(3)]);
+        set(Animation.h3, 'XData', [Animation.new_P3o(1) PUP.r_P3i(1)], 'YData', [Animation.new_P3o(2) PUP.r_P3i(2)], 'ZData', [Animation.new_P3o(3) PUP.r_P3i(3)]);
+        set(Animation.h4, 'XData', [Animation.new_P4o(1) PUP.r_P4i(1)], 'YData', [Animation.new_P4o(2) PUP.r_P4i(2)], 'ZData', [Animation.new_P4o(3) PUP.r_P4i(3)]);
+        set(Animation.h5, 'XData', [Animation.new_P5o(1) PUP.r_P5i(1)], 'YData', [Animation.new_P5o(2) PUP.r_P5i(2)], 'ZData', [Animation.new_P5o(3) PUP.r_P5i(3)]);
+        set(Animation.h6, 'XData', Animation.new_CP_O(1), 'YData', Animation.new_CP_O(2), 'ZData', Animation.new_CP_O(3));
 
         % hold for some time for correct animation speed
         pause(Settings.MaxStep/1e2);
@@ -222,76 +270,89 @@ end
 %% Error test
 
 % check distance error between lower and upper balljoints
-errCalc.FUB = r_out(:,1:3);
-errCalc.FLB = r_out(:,4:6);
+errCalc.FUB = [output.r_P1o_x, output.r_P1o_y, output.r_P1o_z];
+errCalc.FLB = [output.r_P3o_x, output.r_P3o_y, output.r_P3o_z];
 
-dist = zeros(length(errCalc.FUB(:,1)),1);
+errCalc.dist = zeros(length(errCalc.FUB(:,1)),1);
 
 % calculate distance between points for entire simulation
 for n = 1:length(errCalc.FUB(:,1))
-    dist(n) = sqrt( ...
+    errCalc.dist(n) = sqrt( ...
         (errCalc.FUB(n,1) - errCalc.FLB(n,1))^2 + ...
         (errCalc.FUB(n,2) - errCalc.FLB(n,2))^2 + ...
         (errCalc.FUB(n,3) - errCalc.FLB(n,3))^2);
 end
 
 % find error
-err = abs(dist - dist(1));
+errCalc.err = abs(errCalc.dist - errCalc.dist(1));
 
 % plot result
 figure("Name","Error check"); hold all; 
 title({"Error growth rate check", "King pin length change"});
-plot(t, err); grid minor;
+plot(output.t, errCalc.err); grid minor;
 xlabel('sim time (s)');
 ylabel('error (mm)');
 
 %% Constraints checking
 
 % angular velocity
-w_A = [dCamber'; dSpin'; dToe'];
+Check.w_A = [output.dCamber'; output.dSpin'; output.dToe'];
 
 % translational velocity
-v_CP = [dWheelbase'; dTrack'; dTravel'];
+Check.v_CP = [output.dWheelbase'; output.dTrack'; output.dTravel'];
 
-r_P1_CP = r_out(:,1:3)' + PUP.r_FUIF - r_out(:,25:27)';
-r_P2_CP = r_out(:,4:6)' + PUP.r_FUIR  - r_out(:,25:27)';
-r_P3_CP = r_out(:,7:9)' + PUP.r_FLIF  - r_out(:,25:27)';
-r_P4_CP = r_out(:,10:12)' + PUP.r_FLIR  - r_out(:,25:27)';
-r_P5_CP = r_out(:,13:15)' + PUP.r_FIT  - r_out(:,25:27)';
+% contact patch vector
+Check.r_CP = [output.r_CP_O_x'; output.r_CP_O_y'; output.r_CP_O_z'];
 
-test1 = zeros(numel(w_A(1,:)), 1);
-test2 = zeros(numel(w_A(1,:)), 1);
-test3 = zeros(numel(w_A(1,:)), 1);
-test4 = zeros(numel(w_A(1,:)), 1);
-test5 = zeros(numel(w_A(1,:)), 1);
+% outer PUP vectors (from origin)
+Check.r_P1o = [output.r_P1o_x'; output.r_P1o_y'; output.r_P1o_z'];
+Check.r_P2o = [output.r_P2o_x'; output.r_P2o_y'; output.r_P2o_z'];
+Check.r_P3o = [output.r_P3o_x'; output.r_P3o_y'; output.r_P3o_z'];
+Check.r_P4o = [output.r_P4o_x'; output.r_P4o_y'; output.r_P4o_z'];
+Check.r_P5o = [output.r_P5o_x'; output.r_P5o_y'; output.r_P5o_z'];
+
+% outer PUP vectors (from contact patch)
+Check.r_P1_CP = Check.r_P1o - Check.r_CP;
+Check.r_P2_CP = Check.r_P2o - Check.r_CP;
+Check.r_P3_CP = Check.r_P3o - Check.r_CP;
+Check.r_P4_CP = Check.r_P4o - Check.r_CP;
+Check.r_P5_CP = Check.r_P5o - Check.r_CP;
+
+Check.test1 = zeros(numel(Check.w_A(1,:)), 1);
+Check.test2 = zeros(numel(Check.w_A(1,:)), 1);
+Check.test3 = zeros(numel(Check.w_A(1,:)), 1);
+Check.test4 = zeros(numel(Check.w_A(1,:)), 1);
+Check.test5 = zeros(numel(Check.w_A(1,:)), 1);
 
 % find veloctity vectors of each point
-for n = 1%:numel(w_A(1,:))
+for n = 1:numel(Check.w_A(1,:))
 
-    % velocity vectors [NOT CORRECT]
-    v_P1 = v_CP(:,n) + cross(w_A(:,n), r_P1_CP(:,n));
-    v_P2 = v_CP(:,n) + cross(w_A(:,n), r_P2_CP(:,n));
-    v_P3 = v_CP(:,n) + cross(w_A(:,n), r_P3_CP(:,n));
-    v_P4 = v_CP(:,n) + cross(w_A(:,n), r_P4_CP(:,n));
-    v_P5 = v_CP(:,n) + cross(w_A(:,n), r_P5_CP(:,n));
+    % velocity vectors
+    Check.v_P1 = Check.v_CP(:,n) + cross(Check.w_A(:,n), Check.r_P1_CP(:,n));
+    Check.v_P2 = Check.v_CP(:,n) + cross(Check.w_A(:,n), Check.r_P2_CP(:,n));
+    Check.v_P3 = Check.v_CP(:,n) + cross(Check.w_A(:,n), Check.r_P3_CP(:,n));
+    Check.v_P4 = Check.v_CP(:,n) + cross(Check.w_A(:,n), Check.r_P4_CP(:,n));
+    Check.v_P5 = Check.v_CP(:,n) + cross(Check.w_A(:,n), Check.r_P5_CP(:,n));
 
     % link vectors
-    r_P1_CH = r_out(n,1:3)' - PUP.r_FUIF;
-    r_P2_CH = r_out(n,4:6)' - PUP.r_FUIR;
-    r_P3_CH = r_out(n,7:9)' - PUP.r_FLIF;
-    r_P4_CH = r_out(n,10:12)' - PUP.r_FLIR;
-    r_P5_CH = r_out(n,13:15)' - PUP.r_FIT;
+    Check.r_P1_CH = Check.r_P1o(:,n) - PUP.r_P1i;
+    Check.r_P2_CH = Check.r_P2o(:,n) - PUP.r_P2i;
+    Check.r_P3_CH = Check.r_P3o(:,n) - PUP.r_P3i;
+    Check.r_P4_CH = Check.r_P4o(:,n) - PUP.r_P4i;
+    Check.r_P5_CH = Check.r_P5o(:,n) - PUP.r_P5i;
 
     % dot product (should equal zero)
-    test1(n) = v_P1'*r_P1_CH;
-    test2(n) = v_P2'*r_P2_CH;
-    test3(n) = v_P3'*r_P3_CH;
-    test4(n) = v_P4'*r_P4_CH;
-    test5(n) = v_P5'*r_P5_CH;
+    Check.test1(n) = Check.v_P1'*Check.r_P1_CH;
+    Check.test2(n) = Check.v_P2'*Check.r_P2_CH;
+    Check.test3(n) = Check.v_P3'*Check.r_P3_CH;
+    Check.test4(n) = Check.v_P4'*Check.r_P4_CH;
+    Check.test5(n) = Check.v_P5'*Check.r_P5_CH;
 end
 
-disp(['max constraint offset (P1): ', num2str(round(max(abs(test1)), 3, "significant"))]);
-disp(['max constraint offset (P2): ', num2str(round(max(abs(test2)), 3, "significant"))]);
-disp(['max constraint offset (P3): ', num2str(round(max(abs(test3)), 3, "significant"))]);
-disp(['max constraint offset (P4): ', num2str(round(max(abs(test4)), 3, "significant"))]);
-disp(['max constraint offset (P5): ', num2str(round(max(abs(test5)), 3, "significant"))]);
+clear n;
+
+disp(['max constraint offset (P1): ', num2str(round(max(abs(Check.test1)), 3, "significant"))]);
+disp(['max constraint offset (P2): ', num2str(round(max(abs(Check.test2)), 3, "significant"))]);
+disp(['max constraint offset (P3): ', num2str(round(max(abs(Check.test3)), 3, "significant"))]);
+disp(['max constraint offset (P4): ', num2str(round(max(abs(Check.test4)), 3, "significant"))]);
+disp(['max constraint offset (P5): ', num2str(round(max(abs(Check.test5)), 3, "significant"))]);
---
+++ example_data/Suh_PUP.m
@@ -0,0 +1,41 @@
+function [Car, PUP] = Suh_PUP(Settings)
+    
+    % set static ride height
+    Car.RH = 40;  % mm
+
+    if Settings.Axle == "Front"
+
+        % contact patch
+        PUP.r_CP_O    = [ -54.137;     718.938;   -319.000];
+        
+        Car.w_tr_f = PUP.r_CP_O(2);
+        
+        % wheel centre
+        PUP.r_WC_O    = [ -54.137;     718.938;    -45.000];
+        
+        % suspension PUPs
+        PUP.r_P1o   = [ -49.496;     603.805;     71.853];
+        PUP.r_P2o   = [ -49.496;     603.805;     71.853];
+        PUP.r_P3o   = [ -58.341;     624.368;   -150.141];
+        PUP.r_P4o   = [ -58.341;     624.368;   -150.141];
+        PUP.r_P5o   = [-174.166;     633.511;    -98.290];
+        PUP.r_P1i   = [-127.000;     383.250;    106.200];
+        PUP.r_P2i   = [  32.000;     383.250;     95.200];
+        PUP.r_P3i   = [-358.000;     314.750;    -67.600];
+        PUP.r_P4i   = [ -55.650;     232.200;   -110.650];
+        PUP.r_P5i   = [-158.000;     360.500;    -66.600];
+        
+        % correct Z coordinate
+        fn = fieldnames(PUP);
+        for n = 1:numel(fn)
+            PUP.(fn{n})(3) = PUP.(fn{n})(3) + 319.000;
+        end
+
+
+    elseif Settings.Axle == "Rear"
+        disp("Rear axle not supported for this dataset");
+
+    else
+        disp("Invalid axle setting");
+    end
+end
